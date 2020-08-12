29e1a1feb595b027d345bd50b679dc867cdd654b
filsino <andimarafioti@gmail.com>
2020-08-12 15:13:47 +0200

    worked a bit on the figure

--- plots/plotConvergence.m
+++ plots/plotConvergence.m
@@ -7,13 +7,13 @@ figure(index);
 %First
 hold on
 for i=1:size(means,3)
-    plot(1:size(means, 2), means(1, :, i),'LineWidth',4)
+    plot(10:100/size(means, 2):100, means(1, :, i),'LineWidth',4)
 end
 
-xlabel('iterations * 10','FontSize',24)
+xlabel('iterations','FontSize',24)
 ylabel(ylabelstr,'FontSize',24)
-set(gca, 'XTick', [1, 4, 10])
-legend({'red = 32','red = 16', 'red = 8', 'red = 4', 'red = 2'},'Location','southeast','FontSize',24)
+set(gca, 'XTick', [1e1, 5e1, 1e2])
+%legend({'red = 32','red = 16', 'red = 8', 'red = 4', 'red = 2'},'Location','southeast','FontSize',24)
 set(gca,'Fontsize',24);
 
 ylim(ylimrange)
