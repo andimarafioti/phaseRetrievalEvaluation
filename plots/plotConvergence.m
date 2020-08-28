da7ae4fc470edeaf41936b5908306cc9a7aea705
amarafioti <andimarafioti@gmail.com>
2020-08-28 17:05:46 +0200

    adapted plot for the convergence

--- plots/plotConvergence.m
+++ plots/plotConvergence.m
@@ -1,19 +1,21 @@
 
 function plotConvergence(index, means, red, titlestr, ylabelstr, ylimrange)
 sr = 22050;
+colors = [[0, 0.4470, 0.7410]; [0.8500, 0.3250, 0.0980]; [0.9290, 0.6940, 0.1250]; [0.4940, 0.1840, 0.5560]; [0.4660, 0.6740, 0.1880]];
 
-figure(index);
+f = figure(index);
+set(f, 'Position', [10 10 900 600])
 
 %First
 hold on
 for i=1:size(means,3)
-    plot(10:100/size(means, 2):100, means(1, :, i),'LineWidth',4)
+    plot(10:100/size(means, 2):100, means(1, :, i),'LineWidth',4, 'Color', colors(i, :))
 end
 
 xlabel('iterations','FontSize',24)
 ylabel(ylabelstr,'FontSize',24)
 set(gca, 'XTick', [1e1, 5e1, 1e2])
-%legend({'red = 32','red = 16', 'red = 8', 'red = 4', 'red = 2'},'Location','southeast','FontSize',24)
+legend({'red = 32','red = 16', 'red = 8', 'red = 4', 'red = 2'},'Location','southeast','FontSize',24)
 set(gca,'Fontsize',24);
 
 ylim(ylimrange)
