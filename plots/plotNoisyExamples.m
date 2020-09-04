7405faf6c6ebd1701bf7a09b97e3b01dcf729141
amarafioti <andimarafioti@gmail.com>
2020-09-04 14:30:46 +0200

    adapted scrip to plot the noisy examples

--- plots/plotNoisyExamples.m
+++ plots/plotNoisyExamples.m
@@ -1,3 +1,5 @@
+clear all
+
 load('random_1_ljspeech.mat')
 
 SNR_1 = SNR;
@@ -15,6 +17,7 @@ to_save_01 = to_save;
 
 sr = 22050;
 tfrs = (M.^2./(sr.*red)')';
+tfrs = tfrs(:, 1:2:length(red));
 
 means_SC_01 = mean(SNR_01, 1);
 means_SC_05 = mean(SNR_05, 1);
@@ -23,16 +26,20 @@ means_SC_1 = mean(SNR_1, 1);
 means_ODG_01 = mean(to_save_01, 1);
 means_ODG_05 = mean(to_save_05, 1);
 means_ODG_1 = mean(to_save_1, 1);
-
+m
 means_SC = [means_SC_01;means_SC_05;means_SC_1];
+means_SC = means_SC(:, :, 1:2:length(red));
 means_ODG = [means_ODG_01; means_ODG_05; means_ODG_1];
+means_ODG = means_ODG(:, :, 1:2:length(red));
 
-colors = [[0, 0.4470, 0.7410]; [0.8500, 0.3250, 0.0980]; [0.9290, 0.6940, 0.1250]; [0.4940, 0.1840, 0.5560]];
+Markers = {'d','+','s','>','<','^', 'o','*','x','v',};
 
-for index = 1:2:length(red)
-    plotNoiseInfluence(1, means_SC(:, :, index), colors((index+1)/2, :), M, tfrs(:, index), 'Noisy influence at different redundancies', 'SC PGHI', [-50, 0])
+for index = 1:3
+    index = 4-index;
+    plotNoiseInfluence(1, squeeze(means_SC(index, :, :)), M, tfrs, 'Noisy influence at different redundancies', 'SC PGHI', [-50, 0], Markers{index})
 end
 
-for index = 1:2:length(red)
-    plotNoiseInfluence(1, means_ODG(:, :, index), colors((index+1)/2, :), M, tfrs(:, index), 'Noisy influence at different redundancies', 'PEAQ PGHI', [-4, 0.5])
+for index = 1:3
+    index = 4-index;
+    plotNoiseInfluence(2, squeeze(means_ODG(index, :, :)), M, tfrs, 'Noisy influence at different redundancies', 'PEAQ PGHI', [-4, 0.5], Markers{index})
 end
