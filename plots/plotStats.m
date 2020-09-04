6d69ba3c77b9d325fc866ae402d54ab35b698ba0
amarafioti <andimarafioti@gmail.com>
2020-09-04 16:20:37 +0200

    commented marks

--- plots/plotStats.m
+++ plots/plotStats.m
@@ -12,15 +12,15 @@ for i=1:size(means,3)
     semilogx(M, means(1, :, i),'LineWidth',4)
 end
 
-
-semilogx(1024, means(1, find(M==1024), 1), '+','LineWidth',3, 'Color','black','MarkerSize',20)
-semilogx(1024, means(1, find(M==1024), 2), '+','LineWidth',3, 'Color','black','MarkerSize',20)
-semilogx(512, means(1, find(M==512), 3), '+','LineWidth',3, 'Color','black','MarkerSize',20)
-
-if size(means, 3) > 4
-    semilogx(512, means(1, find(M==512), 4), '+','LineWidth',3, 'Color','black','MarkerSize',20)
-    semilogx(256, means(1, find(M==256), 5), '+','LineWidth',3, 'Color','black','MarkerSize',20)
-end
+% 
+% semilogx(1024, means(1, find(M==1024), 1), '+','LineWidth',3, 'Color','black','MarkerSize',20)
+% semilogx(1024, means(1, find(M==1024), 2), '+','LineWidth',3, 'Color','black','MarkerSize',20)
+% semilogx(512, means(1, find(M==512), 3), '+','LineWidth',3, 'Color','black','MarkerSize',20)
+% 
+% if size(means, 3) > 4
+%     semilogx(512, means(1, find(M==512), 4), '+','LineWidth',3, 'Color','black','MarkerSize',20)
+%     semilogx(256, means(1, find(M==256), 5), '+','LineWidth',3, 'Color','black','MarkerSize',20)
+% end
 xlabel('M','FontSize',24)
 ylabel(ylabelstr,'FontSize',24)
 set(gca, 'XTick', [1e2, 1e3, 1e4, 1e5])
@@ -42,14 +42,14 @@ for i=1:size(means,3)
     semilogx(tfrs(:, i), means(1, :, i),'LineWidth',4)
 end
 
-semilogx(M(find(M==1024))^2/(sr*32), means(1, find(M==1024), 1), '+','LineWidth',3, 'Color','black','MarkerSize',20)
-semilogx(M(find(M==1024))^2/(sr*16), means(1, find(M==1024), 2), '+','LineWidth',3, 'Color','black','MarkerSize',20)
-semilogx(M(find(M==512))^2/(sr*8), means(1, find(M==512), 3), '+','LineWidth',3, 'Color','black','MarkerSize',20)
-
-if size(means, 3) > 4
-    semilogx(M(find(M==512))^2/(sr*4), means(1, find(M==512), 4), '+','LineWidth',3, 'Color','black','MarkerSize',20)
-    semilogx(M(find(M==256))^2/(sr*2), means(1, find(M==256), 5), '+','LineWidth',3, 'Color','black','MarkerSize',20)
-end
+% semilogx(M(find(M==1024))^2/(sr*32), means(1, find(M==1024), 1), '+','LineWidth',3, 'Color','black','MarkerSize',20)
+% semilogx(M(find(M==1024))^2/(sr*16), means(1, find(M==1024), 2), '+','LineWidth',3, 'Color','black','MarkerSize',20)
+% semilogx(M(find(M==512))^2/(sr*8), means(1, find(M==512), 3), '+','LineWidth',3, 'Color','black','MarkerSize',20)
+% 
+% if size(means, 3) > 4
+%     semilogx(M(find(M==512))^2/(sr*4), means(1, find(M==512), 4), '+','LineWidth',3, 'Color','black','MarkerSize',20)
+%     semilogx(M(find(M==256))^2/(sr*2), means(1, find(M==256), 5), '+','LineWidth',3, 'Color','black','MarkerSize',20)
+% end
 
 
 xlabel('\lambda','FontSize',24)
