2239b629db79667011afe33a0f7cd077290b4075
amarafioti <andimarafioti@gmail.com>
2020-09-09 17:04:59 +0200

    worked on diffdata test

--- test stft parameters/evalPGHIOnFolder.m
+++ test stft parameters/evalPGHIOnFolder.m
@@ -1,4 +1,4 @@
-function [ODG, SC]= evalPGHIOnFolder(soundfiles, M, red, L)
+function [ODG_PGHI, SC_PGHI, ODG_SPSI, SC_SPSI]= evalPGHIOnFolder(soundfiles, M, red, L)
 
 %% STFT parameters 
 flag = 'timeinv';
@@ -9,24 +9,37 @@ win_SNR = {'gauss',a_SNR*M_SNR/L};
 win_SNR = gabwin(win_SNR,a_SNR,M_SNR,L);
 %% Prepare arrays for results
 
-ODG = zeros(1, length(M), length(red));
-SC = zeros(1, length(M), length(red));
+ODG_PGHI = zeros(1, length(M), length(red));
+SC_PGHI = zeros(1, length(M), length(red));
+
+ODG_SPSI = zeros(1, length(M), length(red));
+SC_SPSI = zeros(1, length(M), length(red));
 
 %% Reconstruct signals
 
 
 for index = 1:length(soundfiles)
-    [signal, fs] = audioread(strcat(soundfiles(index).folder, '\', soundfiles(index).name));
+    try
+        [signal, fs] = audioread(strcat(soundfiles(index).folder, '\', soundfiles(index).name));
+    catch
+        continue
+    end
     signal = resample(signal, 22050, fs);
     fs = 22050;
 
     if length(signal) < L
         continue
     end
-    SC(size(SC, 1)+1, :, :) = 0;
-    ODG(size(ODG, 1)+1,  :, :) = 0;
+    SC_PGHI(size(SC_PGHI, 1)+1, :, :) = 0;
+    ODG_PGHI(size(ODG_PGHI, 1)+1,  :, :) = 0;
+    SC_SPSI(size(SC_SPSI, 1)+1, :, :) = 0;
+    ODG_SPSI(size(ODG_SPSI, 1)+1,  :, :) = 0;
    
     signal = signal(1:L, 1);
+    
+    if max(abs(signal)) > 1
+        signal = signal/max(abs(signal));
+    end
 
     for M0 = M
         for red0 = red
@@ -44,28 +57,41 @@ for index = 1:length(soundfiles)
             c_amp_pghi = pghi(c_amp,gamma,a0,M0,flag, 'tol', 1e-7);
             f_amp_pghi = idgtreal(c_amp_pghi,dual,a0,M0,flag);
 
+            % SPSI
+            c_amp_spsi = spsi(c_amp,a0,M0,flag);
+            f_amp_spsi = idgtreal(c_amp_spsi,dual,a0,M0,flag);
+
             % Measure PEAQ
             signal_resampled = resample(signal(M0:end-M0), 48000, fs);
             pghi_resampled = resample(f_amp_pghi(M0:end-M0), 48000, fs);
+            spsi_resampled = resample(f_amp_spsi(M0:end-M0), 48000, fs);
 
-            audiowrite('or.wav', signal_resampled, 48000);
-            audiowrite('pghi.wav', pghi_resampled, 48000);
+            audiowrite('or_eval.wav', signal_resampled, 48000);
+            audiowrite('pghi_eval.wav', pghi_resampled, 48000);
+            audiowrite('spsi_eval.wav', spsi_resampled, 48000);
 
-            [odg, movb] = PQevalAudio_fn('or.wav', 'pghi.wav');
-            ODG(size(ODG, 1), M0==M, red0==red) = odg;
-            
+            [odg, movb] = PQevalAudio_fn('or_eval.wav', 'pghi_eval.wav');
+            ODG_PGHI(size(ODG_PGHI, 1), M0==M, red0==red) = odg;
+
+            [odg, movb] = PQevalAudio_fn('or_eval.wav', 'spsi_eval.wav');
+            ODG_SPSI(size(ODG_SPSI, 1), M0==M, red0==red) = odg;
+
             % Measure spectral divergence
 
             re_c_amp = abs(dgtreal(signal,win_SNR,a_SNR,M_SNR,L,flag));
             re_c_amp_pghi = abs(dgtreal(f_amp_pghi,win_SNR,a_SNR,M_SNR,L,flag));
+            re_c_amp_spsi = abs(dgtreal(f_amp_spsi,win_SNR,a_SNR,M_SNR,L,flag));
 
-            SC(size(SC, 1), M0==M, red0==red) = magnitudeerrdb(re_c_amp_pghi, re_c_amp);
+            SC_PGHI(size(SC_PGHI, 1), M0==M, red0==red) = magnitudeerrdb(re_c_amp_pghi, re_c_amp);
+            SC_SPSI(size(SC_SPSI, 1), M0==M, red0==red) = magnitudeerrdb(re_c_amp_spsi, re_c_amp);
 
         end
     end
 end
 
-SC = SC(2:end, :, :);
-ODG = ODG(2:end, :, :);
+SC_PGHI = SC_PGHI(2:end, :, :);
+ODG_PGHI = ODG_PGHI(2:end, :, :);
+SC_SPSI = SC_SPSI(2:end, :, :);
+ODG_SPSI = ODG_SPSI(2:end, :, :);
 
 end
--- test stft parameters/testWindowsfor_diff_data.m
+++ test stft parameters/testWindowsfor_diff_data.m
@@ -3,42 +3,37 @@ clear all
 %ltfatstart(); % start the ltfat toolbox
 %phaseretstart;
 
-L = 2^13 * 3 * 5;
+L = 2^11 * 3 * 5;
 
 d = 32* propdiv(L/32);
 
-M = d(find(d>=1024 & d<L/16));
-%M = [32 M];
-red = [32, 16, 8, 4, 2];
+M = d(find(d>=64 & d<L/2));
+M = [32 M];
+red = [32, 8, 2];
 
 tfrs = (M.^2./(L.*red)')';
 
 %%
-examples = 2;
+examples = 128;
 base_folder = '\\kfsnas08.kfs.oeaw.ac.at\Denklast\amarafioti\Documents\Datasets\Lakh\new-simple-piano\';
 soundfiles = findWavFiles(base_folder);
 soundfiles = soundfiles(1:examples);
-[ODG_midi, SC_midi] = evalPGHIOnFolder(soundfiles, M, red, L);
+[ODG_midi_PGHI, SC_midi_PGHI, ODG_midi_SPSI, SC_midi_SPSI] = evalPGHIOnFolder(soundfiles, M, red, L);
 
 base_folder = '\\kfsnas08.kfs.oeaw.ac.at\Denklast\amarafioti\Documents\Datasets\LJSpeech-1.1\wavs\';
 soundfiles = findWavFiles(base_folder);
 soundfiles = soundfiles(1:examples);
-[ODG_speech, SC_speech] = evalPGHIOnFolder(soundfiles, M, red, L);
+[ODG_speech_PGHI, SC_speech_PGHI, ODG_speech_SPSI, SC_speech_SPSI] = evalPGHIOnFolder(soundfiles, M, red, L);
 
 base_folder = '\\kfsnas08.kfs.oeaw.ac.at\Denklast\amarafioti\Documents\Datasets\fma_electronic\';
 soundfiles = findMp3Files(base_folder);
 soundfiles = soundfiles(1:examples);
-[ODG_electronic, SC_electronic] = evalPGHIOnFolder(soundfiles, M, red, L);
+[ODG_electronic_PGHI, SC_electronic_PGHI, ODG_electronic_SPSI, SC_electronic_SPSI] = evalPGHIOnFolder(soundfiles, M, red, L);
 
 base_folder = '\\kfsnas08.kfs.oeaw.ac.at\Denklast\amarafioti\Documents\Datasets\fma_rock\';
 soundfiles = findMp3Files(base_folder);
 soundfiles = soundfiles(1:examples);
-[ODG_rock, SC_rock] = evalPGHIOnFolder(soundfiles, M, red, L);
-
-plotStats(1, mean(ODG_midi, 1), M, tfrs, 'Perceptual quality of phaseless reconstruction', 'PEAQ PGHI', [-4,0.5])
-plotStats(2, mean(SC_midi, 1), M, tfrs, 'Objective quality of phaseless reconstruction', 'SC PGHI', [-60,6])
-plotStats(3, mean(ODG_speech, 1), M, tfrs, 'Perceptual quality of phaseless reconstruction', 'PEAQ FGLA', [-4,0.5])
-plotStats(4, mean(SC_speech, 1), M, tfrs, 'Objective quality of phaseless reconstruction', 'SC FGLA', [-60,6])
+[ODG_rock_PGHI, SC_rock_PGHI, ODG_rock_SPSI, SC_rock_SPSI] = evalPGHIOnFolder(soundfiles, M, red, L);
 
 
 
