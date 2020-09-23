2a024c35db6c91c097672189d59e18c29fbdf9c8
amarafioti <andimarafioti@gmail.com>
2020-09-23 15:53:22 +0200

    adapted tfr

--- plots/plot_hann_tests.m
+++ plots/plot_hann_tests.m
@@ -1,10 +1,10 @@
 load('hann_test_ljspeech.mat')
 sr = 22050;
-tfrs = repmat(window_lengths', [1, 5]);
+tfrs = repmat((( 0.25645*window_lengths.^2)./L)', [1, 5]);
 % tfrs = tfrs*L/sr;
 
-plotThreeMethodComparison(1, [mean(to_save_pghi, 1); mean(to_save_spsi, 1); mean(to_save_gla, 1)], tfrs, 'ODG', ['PGHI';'SPSI';'FGLA'], [-4,0.5])
-plotThreeMethodComparison(2, [-mean(SNR_pghi, 1); -mean(SNR_spsi, 1); -mean(SNR_gla, 1)], tfrs, 'SC', [' ';' ';' '], [0, 60])
+plotThreeMethodComparison(3, [mean(to_save_pghi, 1); mean(to_save_gla, 1); mean(to_save_spsi, 1)], tfrs, 'ODG', ['PGHI';'FGLA';'SPSI'], [-4,0.5])
+plotThreeMethodComparison(4, [-mean(SNR_pghi, 1); -mean(SNR_gla, 1); -mean(SNR_spsi, 1)], tfrs, 'SC', [' ';' ';' '], [0, 60])
 % 
 % plotStats(1, mean(to_save_pghi, 1), tfrs, 'PEAQ PGHI', [-4,0.5])
 % plotStats(2, mean(SNR_pghi, 1), tfrs, 'SC PGHI', [-60,6])
