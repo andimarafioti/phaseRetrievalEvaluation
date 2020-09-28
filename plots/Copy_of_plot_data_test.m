a2d0464db14bbc5ee39d35ba26b04f9b3a0d85e0
filsino <andimarafioti@gmail.com>
2020-09-28 17:26:02 +0200

    make the plots clearer by removing cluttering data

--- plots/Copy_of_plot_data_test.m
+++ plots/Copy_of_plot_data_test.m
@@ -1,4 +1,4 @@
-%load('diff_data_midi_speech.mat')
+load('final_final_diff_data.mat')
 Markers = {'d','+','s','>','<','^', 'o','*','x','v',};
 
 sr = 22050;
@@ -17,20 +17,21 @@ means_ODG_PGHI = [mean(ODG_midi_PGHI, 1); mean(ODG_speech_PGHI, 1); mean(ODG_ele
 means_ODG_SPSI = [mean(ODG_midi_SPSI, 1); mean(ODG_speech_SPSI, 1); mean(ODG_electronic_SPSI, 1); mean(ODG_rock_SPSI, 1)];
 
 colors = [[0, 0.4470, 0.7410]; [0.8500, 0.3250, 0.0980]; [0.9290, 0.6940, 0.1250]; [0.4940, 0.1840, 0.5560]; [0.4660, 0.6740, 0.1880]];
+
 for index = 1:length(red)
-    plotDiffData(1, -means_SC_PGHI(:, :, index), M, tfrs(:, index), 'SC PGHI', [0, 60], colors(2*index-1, :))
+    plotDiffData(1, -means_SC_PGHI(1:3, :, index), M, tfrs(:, index), 'SC PGHI', [0, 60], colors(2*index-1, :))
+end
+
+for index = 2
+    plotDiffData(2, -means_SC_SPSI(1:3, :, index), M, tfrs(:, index), 'SC SPSI', [0, 60], colors(2*index-1, :))
 end
 
 for index = 1:length(red)
-    plotDiffData(2, -means_SC_SPSI(:, :, index), M, tfrs(:, index), 'SC SPSI', [0, 60], colors(2*index-1, :))
+    plotDiffData(3, means_ODG_PGHI(1:3, :, index), M, tfrs(:, index), 'ODG PGHI', [-4, 0.5], colors(2*index-1, :))
 end
 
-for index = 1:length(red)
-    plotDiffData(3, means_ODG_PGHI(:, :, index), M, tfrs(:, index), 'ODG PGHI', [-4, 0.5], colors(2*index-1, :))
-end
-
-for index = 1:length(red)
-    plotDiffData(4, means_ODG_SPSI(:, :, index), M, tfrs(:, index), 'ODG SPSI', [-4, 0.5], colors(2*index-1, :))
+for index = 2
+    plotDiffData(4, means_ODG_SPSI(1:3, :, index), M, tfrs(:, index), 'ODG SPSI', [-4, 0.5], colors(2*index-1, :))
 end
 
 
