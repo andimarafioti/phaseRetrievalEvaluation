429a1030f52aad7dcdaa70b8ca7c6041a14cfef7
filsino <andimarafioti@gmail.com>
2020-10-01 14:15:30 +0200

    changed redundancy for M

--- plots/plotConvergence.m
+++ plots/plotConvergence.m
@@ -9,13 +9,13 @@ set(f, 'Position', [10 10 900 420])
 %First
 hold on
 for i=1:size(means,3)
-    plot(10:200/size(means, 2):200, means(1, :, i),'LineWidth',4, 'Color', colors(i, :))
+    plot(10:100/size(means, 2):100, means(1, :, i),'LineWidth',4, 'Color', colors(i, :))
 end
 
 xlabel('iterations','FontSize',24)
 ylabel(ylabelstr,'FontSize',24)
 set(gca, 'XTick', [1e1, 5e1, 1e2, 1e3])
-legend({'red = 32','red = 16', 'red = 8', 'red = 4', 'red = 2'},'Location','southeast','FontSize',24)
+legend({'D = 32','D = 16', 'D = 8', 'D = 4', 'D = 2'},'Location','southeast','FontSize',24)
 set(gca,'Fontsize',24);
 
 ylim(ylimrange)
