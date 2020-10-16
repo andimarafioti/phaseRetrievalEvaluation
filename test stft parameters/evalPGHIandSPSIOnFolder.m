02687c9c1a511284d71dbc2727045bd1c5eb270f
filsino <andimarafioti@gmail.com>
2020-10-16 18:47:00 +0200

    making clear lambda conventions

--- test stft parameters/evalPGHIandSPSIOnFolder.m
+++ test stft parameters/evalPGHIandSPSIOnFolder.m
@@ -5,7 +5,7 @@ flag = 'timeinv';
 
 M_SNR = 2048;
 a_SNR = 128;
-win_SNR = {'gauss',a_SNR*M_SNR/L};
+win_SNR = {'gauss',a_SNR*M_SNR/L}; % Lambda differs from the definition in the paper because LTFAT's gaussian is defined differently
 win_SNR = gabwin(win_SNR,a_SNR,M_SNR,L);
 %% Prepare arrays for results
 
@@ -45,7 +45,7 @@ for index = 1:length(soundfiles)
         for red0 = red
             a0 = M0 / red0;
 
-            win = {'gauss',a0*M0/L};
+            win = {'gauss',a0*M0/L}; % Lambda differs from the definition in the paper because LTFAT's gaussian is defined differently
             win = gabwin(win,a0,M0,L);
             gamma = a0*M0;
             dual = {'dual',win};
