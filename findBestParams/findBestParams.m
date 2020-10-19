07b336d873dad88a8cb1330375380e3f5a738007
filsino <andimarafioti@gmail.com>
2020-10-19 11:25:08 +0200

    finished coding the logic to pick the best params, and fuxed the string array loop

--- findBestParams/findBestParams.m
+++ findBestParams/findBestParams.m
@@ -7,13 +7,16 @@ M_SNR = 2048;
 a_SNR = 128;
 win_SNR = {'gauss',a_SNR*M_SNR/L};
 win_SNR = gabwin(win_SNR,a_SNR,M_SNR,L);
+flag = 'timeinv';
 
-for signal_path = array_of_signal_paths
-   [signal, sr] = audioread(signal_path);
+index = 0;
+for signal_num = 1:length(array_of_signal_paths)
+   [signal, sr] = audioread(array_of_signal_paths(signal_num));
    
    if length(signal) < L
         continue
    end
+   index = index + 1;
    
    signal = signal(1:L);
 
@@ -34,8 +37,8 @@ for signal_path = array_of_signal_paths
             f_amp_phase_ret = idgtreal(c_amp_phase_ret,dual,a0,M0,flag);
 
             % Measure PEAQ
-            signal_resampled = resample(signal(M0:end-M0), 48000, fs);
-            phase_ret_resampled = resample(f_amp_phase_ret(M0:end-M0), 48000, fs);
+            signal_resampled = resample(signal(M0:end-M0), 48000, sr);
+            phase_ret_resampled = resample(f_amp_phase_ret(M0:end-M0), 48000, sr);
 
             audiowrite('or_findBestParams.wav', signal_resampled, 48000);
             audiowrite('phaseret_findBestParams.wav', phase_ret_resampled, 48000);
@@ -53,21 +56,24 @@ for signal_path = array_of_signal_paths
         end
     end
 end
+
+ODGs = ODGs(1:index, :, :); % erase unused values caused by short files
+SNRs_ms = SNRs_ms(1:index, :, :);
  
-mean_ODGs = mean(ODGs, 1);
-max_ODGs = max(mean_ODGs);
+mean_ODGs = squeeze(mean(ODGs, 1)); % remove meaned dimension
+max_ODGs = max(mean_ODGs, [], 'all');
 % good_ODGs = mean_ODGs(mean_ODGs > max_ODGs-odg_threshold);
 
-mean_SNRs_ms = mean(SNRs_ms, 1);
-min_SNRs_ms = min(mean_SNRs_ms);
+mean_SNRs_ms = squeeze(mean(SNRs_ms, 1));
+min_SNRs_ms = min(mean_SNRs_ms, [], 'all');
 % good_SNRs_ms = mean_SNRs_ms(mean_SNRs_ms < min_SNRs_ms + SNR_threshold);
 
-good_on_both_measures = find((mean_ODGs > max_ODGs-odg_threshold) & (mean_SNRs_ms < min_SNRs_ms + SNR_threshold));
+good_on_both_measures = (mean_ODGs > max_ODGs-odg_threshold) & (mean_SNRs_ms < min_SNRs_ms + SNR_threshold);
 
-rep_M = repmat(Ms, [length(Ms), length(Ds)]);
-rep_Ds = repmat(Ds, [length(Ms), length(Ds)]);
+rep_M = repmat(Ms, [length(Ds), 1]);
+rep_Ds = repmat(Ds, [length(Ms), 1]);
 
-bestMs = nonzeros(rep_M(good_on_both_measures));
+bestMs = nonzeros(rep_M(good_on_both_measures'));
 bestDs = nonzeros(rep_Ds(good_on_both_measures));
 
 end
