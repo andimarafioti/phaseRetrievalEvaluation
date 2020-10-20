25f688020e499106572a6892126b7d127616e1ba
amarafioti <andimarafioti@gmail.com>
2020-10-20 17:15:56 +0200

    changed params for every phase retrieval algorithm (fgla doesnt need redundancies over 8

--- findBestParams/testFindBestParams.m
+++ findBestParams/testFindBestParams.m
@@ -5,7 +5,7 @@ clear all
 
 base_folder = '\\kfsnas08.kfs.oeaw.ac.at\Denklast\amarafioti\Documents\Datasets\LJSpeech-1.1\wavs\';
 soundfiles = dir(base_folder);
-ssoundfiles = soundfiles(3:end);
+soundfiles = soundfiles(3:end);
 examples = 64;
 array_of_signal_paths = strings(examples,1);
 
@@ -18,13 +18,21 @@ L = 2^13 * 3 * 5;
 
 d = 32* propdiv(L/32);
 
-Ms = d(find(d>=128 & d<L/8));
+Ms = d(find(d>=256 & d<L/16));
 Ds = [32, 16, 8, 4];
 
-odg_threshold = 0.5;
-SNR_threshold = 10;
+odg_threshold = 0.3;
+SNR_threshold = 15;
 
 [best_M_pghi, best_D_pghi] = findBestParams(@pghi_func, array_of_signal_paths, Ms, Ds, L, odg_threshold, SNR_threshold);
+
+
+Ms = d(find(d>=128 & d<L/16));
+Ds = [8, 4];
+
+odg_threshold = 0.3;
+SNR_threshold = 10;
+
 [best_M_fgla, best_D_fgla] = findBestParams(@fgla_func, array_of_signal_paths, Ms, Ds, L, odg_threshold, SNR_threshold);
 [best_M_spsi, best_D_spsi] = findBestParams(@spsi_func, array_of_signal_paths, Ms, Ds, L, odg_threshold, SNR_threshold);
 
