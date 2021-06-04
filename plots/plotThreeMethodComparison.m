acfec452461cc9a85e46f8cf3740411b69e0285d
andimarafioti <andimarafioti@gmail.com>
2021-06-04 16:43:15 +0200

    removed tight subplot and used normal subplots instead

--- plots/plotThreeMethodComparison.m
+++ plots/plotThreeMethodComparison.m
@@ -4,10 +4,9 @@ sr = 22050;
 colors = [[0, 0.4470, 0.7410]; [0.8500, 0.3250, 0.0980]; [0.9290, 0.6940, 0.1250]; [0.4940, 0.1840, 0.5560]; [0.4660, 0.6740, 0.1880]];
 
 f = figure(index);
-set(f, 'Position', [10 10 1700 700])
-[ha, pos] = tight_subplot(1, 3, [0.1 .1],[.15 .1],[.1 .02]);
+set(f, 'Position', [10 10 1200 450])
 for j=1:size(means,1)
-    axes(ha(j));
+    ha = subplot(1, 3, j);
     set(gca, 'XScale', 'log');
     set(gca,'Fontsize',48);
     xlim([1e-3, 2e4])
@@ -21,12 +20,22 @@ for j=1:size(means,1)
     for i=1:size(means,3)
         semilogx(tfrs(:, i), means(j, :, i),'LineWidth', 9, 'Color', colors(i, :))
     end
+%     set(gca,'xtick',[])
+%     set(gca,'xticklabel',[])
+
     if j == 1
         ylabel(ylabelstr,'FontSize',48)
+    end
+
+    if j == 2
+        set(gca,'ytick',[])
+        set(gca,'yticklabel',[])
     end
     
     if j == 3 
         legend({'D = 32','D = 16', 'D = 8', 'D = 4', 'D = 2'},'Location','northeast','FontSize',32)
+        set(gca,'ytick',[])
+        set(gca,'yticklabel',[])
     end
 end
 
