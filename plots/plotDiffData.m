bd3ef8c7276bc8b81237848b755d6565bb5da5fc
andimarafioti <andimarafioti@gmail.com>
2021-06-07 14:56:50 +0200

    adapted figure to match fig 6 and 7

--- plots/plotDiffData.m
+++ plots/plotDiffData.m
@@ -2,7 +2,7 @@ function plotDiffData(index, means, M, tfrs, ylabelstr, titles, ylimrange, faceC
 sr = 22050;
 
 f = figure(index);
-set(f, 'Position', [0 0 750 1000])
+set(f, 'Position', [0 0 600 800])
 
 hold on
 set(gca, 'XScale', 'log');
@@ -17,23 +17,26 @@ X = [tfrs tfrs];
 
 line_styles = {'-'; '--'; ':'; '-.'};
 
-plot(NaN,NaN,line_styles{1},'Color', faceColor,'LineWidth',3);
-plot(NaN,NaN,line_styles{2},'Color', faceColor,'LineWidth',3);
-plot(NaN,NaN,line_styles{3},'Color', faceColor,'LineWidth',3);
+plot(NaN,NaN,line_styles{1},'Color', faceColor,'LineWidth',6);
+plot(NaN,NaN,line_styles{2},'Color', faceColor,'LineWidth',6);
+plot(NaN,NaN,line_styles{3},'Color', faceColor,'LineWidth',6);
 
 for i=1:size(means,1)
     semilogx(tfrs(:), means(i, :), 'LineWidth',9, 'LineStyle', line_styles{i}, 'Color', faceColor)
 end
 
 xlim([1e-3, 2e4])
-set(gca, 'XTick', [1e-3,1e-1,1e1,1e3])
+set(gca, 'XTick', [1e-3, 1e-1,1e1,1e3])
+% set(gca, 'YTick', [])
 
 ylim(ylimrange)
 xlabel('\lambda','FontSize',48)
 ylabel(ylabelstr,'FontSize',48)
 title(titles,'FontSize',48)
 
-%legend({'MIDI','Speech', 'Music'},'Location','northwest','FontSize',48)
+% leg = legend({'MIDI','Speech', 'Music'},'Location','northeast','FontSize',32);
+% leg.ItemTokenSize = [80,160];
+
 set(gca,'Fontsize',48);
 
 % hold off
